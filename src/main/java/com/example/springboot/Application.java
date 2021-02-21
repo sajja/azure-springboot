793e73deb2cdb575edc348ff230e48c90079ad66
Sajith Silva <sajiths@pagero.com>
2021-02-21 19:02:41 +0530

    Fixed run error

--- src/main/java/com/example/springboot/Application.java
+++ src/main/java/com/example/springboot/Application.java
@@ -23,7 +23,7 @@ public class Application {
 //    }
 //
     public static void main(String[] args) {
-        SpringApplication.run(HelloController.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
     @Bean
